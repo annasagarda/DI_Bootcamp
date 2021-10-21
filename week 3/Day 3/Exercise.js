80ab01083c59e429237d9854030dcffff7307af0
Anna Sagarda <anna.sagarda@gmail.com>
2021-10-21 16:16:44 +0300

    Add Week 3 Day 4 Exercise XP

--- week 3/Day 3/Exercise.js
+++ week 3/Day 3/Exercise.js
@@ -2,16 +2,12 @@
 
 //Exercise 1
 
-let fruits = ["Banana", "Apples", "Oranges", "Blueberries"]
+let age = parseInt(prompt("how old are u"));
 
-fruits.shift()
-fruits.sort()
-fruits.push("Kiwi")
-fruits.splice(0,1)
-fruits.reverse()
-
-//Exercise 2
-
-let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
-let a = moreFruits[1][1]
-console.log(a)
+if (age>18) {
+	alert("Ok, Let's go")
+} else if (age<18) {
+	alert("sorry, you are to young to drive this car")
+} else {
+	alert("Welcome to your first driving year")
+}
---
+++ week 3/Day 4/Exercise XP.js
@@ -0,0 +1,54 @@
+//Exercise 1//
+
+let x = parseInt(prompt("type the x value"));
+let y = parseInt(prompt("type the y value"));
+
+if (x>y) {alert("x is the biggest number")}
+else { alert("try again")}
+
+//Exercise 2//
+
+let newDog = prompt("type in your favourite dog breed") //"Chihuahua"
+
+console.log(newDog.length);
+
+console.log(newDog.toUpperCase());
+
+console.log(newDog.toLowerCase());
+
+if (newDog.toLowerCase() === "chihuahua") {alert ("I love Chihuahuas, it’s my favorite dog breed")}
+ else {alert("I don't care, I prefer rabbits")};
+
+//Exercise 3//
+
+let x = prompt("Give me a natural number");
+var num1 = parseInt(x);
+function isNaturalNumber (str) {
+    var pattern = /^(0|([1-9]\d*))$/;
+    return pattern.test(str);
+}
+
+if (isNaturalNumber(x) && num1%2 === 0 && num1!==0) {alert("x is an even number")}
+	else  if (isNaturalNumber(x) && num1%2 !==0 && num1 !==0) {alert("x is an odd number")}
+		else {alert ("This number isn't natural")}
+
+
+//Exercise 4//
+
+let me = ["my", "favourite", "color", "is", "blue"]
+
+console.log(me.join(' '))
+
+let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+
+users=["Lea123", "Princess45","Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+
+
+
+if (users.length === 0) {alert ("no one is online")}
+
+	else if(users.length === 1) {alert (users[0]+" is online")}
+		else if (users.length === 2) {alert (users[0]+ " and " + users[1] 
+			+ "are online")}
+			else if (users.length >2) {alert (users[0] + "," + users[1] + " and " +
+				(users.length-2) + " more users are online")}
